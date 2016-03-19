a21195c7bd086eb4f87d4a90d6fcece51c61e626
a.sarafian <dev1978@outlook.com>
2016-03-19 21:19:51 +0100

    Query operating system

--- Powershell/Scripts/VSS/QueryEnvironment.ps1
+++ Powershell/Scripts/VSS/QueryEnvironment.ps1
@@ -7,3 +7,6 @@ Write-Verbose "PSModulePath"
 $env:PSModulePath -split ';'
 
 Write-Verbose "env:USERNAME=$($env:USERNAME)"
+
+$os=Get-WmiObject Win32_OperatingSystem
+Write-Verbose "$($os.Name) ($($os.OSArchitecture))"
