e1290c3c30074e11022324998d870040fd993fd0
a.sarafian <dev1978@outlook.com>
2016-03-19 22:17:51 +0100

    Fix

--- Powershell/Pester/Test-Basic.ps1
+++ Powershell/Pester/Test-Basic.ps1
@@ -4,7 +4,6 @@ $testPath=Resolve-Path "$PSScriptRoot\..\Modules\Basic\"
 if(Test-VSSAgent)
 {
     $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".").Replace(".ps1", "")
-    . "$here\$sut"    
     Invoke-Pester -CodeCoverage *.ps1 -Path "$testPath" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\$sut.xml"
 }
 else
