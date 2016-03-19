7a1bbf852222702c29181c78f40e8aeb813505e8
a.sarafian <dev1978@outlook.com>
2016-03-19 21:39:39 +0100

    Checking variables

--- Powershell/Scripts/VSS/QueryEnvironment.ps1
+++ Powershell/Scripts/VSS/QueryEnvironment.ps1
@@ -10,3 +10,11 @@ Write-Verbose "env:USERNAME=$($env:USERNAME)"
 
 $os=Get-WmiObject Win32_OperatingSystem
 Write-Verbose "$($os.Name) ($($os.OSArchitecture))"
+
+Write-Verbose "$($os.Name) ($($os.OSArchitecture))"
+
+Write-Verbose "Env:system.debug=$($Env:system.debug)"
+Write-Verbose "Env:system_debug=$($Env:system_debug)"
+Write-Verbose "system.debug=$system.debug"
+Write-Verbose "system_debug=$system_debug"
+
