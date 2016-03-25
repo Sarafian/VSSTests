4f333eec177731dd5e0dfcacea7575517b53a1c3
Alex Sarafian <asarafian@sdl.com>
2016-03-25 15:22:24 +0100

    attempt

--- Powershell/Pester/Initialize-PSMarkdownDependency.ps1
+++ Powershell/Pester/Initialize-PSMarkdownDependency.ps1
@@ -30,35 +30,36 @@ if(-not (Get-Command ConvertTo-Markdown -ErrorAction SilentlyContinue)) {
     #>
     Write-Verbose "Creating module structure in $targetPath"
 
-    $modulePath=Join-Path $env:TEMP "Modules\PSMarkdown"
-    if(-not (Test-Path $modulePath))
+    $modulesPath=Join-Path $env:TEMP "Modules"
+    $psMarkdownDirectoryPath=Join-Path $modulesPath "PSMarkdown"
+    if(-not (Test-Path $psMarkdownDirectoryPath))
     {
-        New-Item $modulePath -ItemType Directory
+        New-Item $psMarkdownDirectoryPath -ItemType Directory
     }
     $wc = New-Object System.Net.WebClient
 
     $url='https://raw.githubusercontent.com/ishu3101/PSMarkdown/master/ConvertTo-Markdown.ps1'
-    $ps1Path = Join-Path $modulePath "ConvertTo-Markdown.ps1"
+    $ps1Path = Join-Path $psMarkdownDirectoryPath "ConvertTo-Markdown.ps1"
     Write-Verbose "Downloading $url to $ps1Path"
     $wc.DownloadFile($url, $ps1Path)
 
-    $psm1Path=Join-Path $modulePath "PSMarkdown.psm1"
+    $psm1Path=Join-Path $psMarkdownDirectoryPath "PSMarkdown.psm1"
     Write-Verbose "Writing $psm1Path"
     '. $PSScriptRoot\ConvertTo-Markdown.ps1' | Out-File $psm1Path
 
-    $items=Get-ChildItem $modulePath -Recurse -File
-    Write-Verbose "Content in $modulePath"
+    $items=Get-ChildItem $modulesPath -Recurse -File
+    Write-Verbose "Content in $modulesPath"
     $items |ForEach-Object { 
         Write-Verbose $_.FullName
         $content=Get-Content $_.FullName
         Write-Verbose $content
     }
     
-    $env:PSModulePath+=';'+$modulePath
+    $env:PSModulePath+=';'+$modulesPath
     Write-Verbose "PSModulePath is"
     ($env:PSModulePath -split ';')|ForEach-Object { Write-Verbose $_}
     Write-Verbose "Importing module"
-    Import-Module PSMarkdown -Verbose
-    Get-Command -Module PSMarkdown -Verbose
+    Import-Module PSMarkdown -Verbose -Debug
+    (Get-Module PSMarkdown).Path
 }
 
